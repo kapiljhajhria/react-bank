fcc4fe22070e6e8be31265140e4146d7ea80ab21
kapil jhajhria <jhajhria44@gmail.com>
2021-02-20 14:02:15 +0530

    info sections added

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,56 @@
+export const HomeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-2.svg").default,
+  alt: "Car",
+  dark: true,
+  dark2: false,
+  primary: true,
+  darkText: false,
+};
+
+export const HomeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account any time",
+  description:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or a computer.",
+  buttonLabel: "learn More",
+  imgStart: true,
+  img: require("../../images/svg-3.svg").default,
+  alt: "Piggybank",
+  dark: false,
+  dark2: false,
+  primary: false,
+  darkText: true,
+};
+
+export const HomeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you are ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-4.svg").default,
+  alt: "Papers",
+  dark: false,
+  dark2: false,
+  primary: false,
+  darkText: true,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import Navbar from "../components/NavBar";
 import { SideBar } from "../components/SideBar/index";
 import { HeroSection } from "../components/HeroSection/index";
+import { InfoSection } from "../components/InfoSection/index";
+import {
+  HomeObjOne,
+  HomeObjTwo,
+  HomeObjThree,
+} from "../components/InfoSection/Data";
 
 export const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,6 +20,9 @@ export const Home = () => {
       <SideBar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection {...HomeObjOne} />
+      <InfoSection {...HomeObjTwo} />
+      <InfoSection {...HomeObjThree} />
     </>
   );
 };
