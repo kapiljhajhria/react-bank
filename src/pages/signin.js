dbed2ae7ea69be0d9dc6d819f85bef1c07379dfb
kapil jhajhria <jhajhria44@gmail.com>
2021-02-20 17:45:53 +0530

    signin page added

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SignIn from "../components/Signin/index";
 
 export const SigninPage = () => {
   return (
-    <div>
-      <h1>Sign in page</h1>
-    </div>
+    <>
+      <SignIn />
+    </>
   );
 };
