9a1e33d17be93e4cc88210029989e89c7bf16da5
kapil jhajhria <jhajhria44@gmail.com>
2021-02-20 17:48:20 +0530

    scroll to top on changing page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SignIn from "../components/Signin/index";
+import ScrollToTop from "../components/ScrollToTop";
 
 export const SigninPage = () => {
   return (
     <>
+      <ScrollToTop />
       <SignIn />
     </>
   );
